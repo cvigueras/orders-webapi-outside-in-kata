01ae3277bfc5ee1736802dd403d4c822de84f87b
cvigueras <carlosj.vigueras@gmail.com>
2023-05-10 14:43:44 +0200

    Chore: Init project

---
+++ OrdersWeb.Api/Start/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SQLite;
+
+namespace OrdersWeb.Api.Start;
+
+public class Startup
+{
+    public IConfiguration ConfigRoot
+    {
+        get;
+    }
+
+    public Startup(IConfiguration configuration)
+    {
+        ConfigRoot = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
---
+++ OrdersWeb.Test/StartupTest.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OrdersWeb.Api;
+using System.Data.SQLite;
+
+namespace OrdersWeb.Test;
+
+public class StartupTest : WebApplicationFactory<Program>
+{
+    private readonly SQLiteConnection? _connection;
+
+    public StartupTest()
+    {
+        _connection = new SQLiteConnection("Data Source=:memory:");
+
+        _connection.Open();
+
+        CreateDataBase();
+    }
+
+    private void CreateDataBase()
+    {
+        
+    }
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddSingleton(_connection);
+        });
+
+        return base.CreateHost(builder);
+    }
+
+    public SQLiteConnection? GetConnection()
+    {
+        return _connection;
+    }
+}
