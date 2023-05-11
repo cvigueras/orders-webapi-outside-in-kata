4c957ad6f40a1e39b284dbf75bc53fa412b16df0
cvigueras <carlosj.vigueras@gmail.com>
2023-05-11 15:54:55 +0200

    Refactor: grouped get connection and created database with StartupTest.

--- OrdersWeb.Test/OrderRepositoryShould.cs
+++ OrdersWeb.Test/OrderRepositoryShould.cs
@@ -7,17 +7,18 @@ namespace OrdersWeb.Test
 {
     public class OrderRepositoryShould
     {
+        private SQLiteConnection? connection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var startupTest = new StartupTest();
+            connection = startupTest.GetConnection();
+        }
+
         [Test]
         public void RetrieveEmptyOrderWhenNotExists()
         {
-            var connection = new SQLiteConnection("Data Source=:memory:");
-            connection.Open();
-            connection.Execute(@"CREATE TABLE IF NOT EXISTS Orders(
-                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                Customer VARCHAR(200) NOT NULL,
-                Address VARCHAR(400) NOT NULL,
-                Number VARCHAR(10) NOT NULL)"
-            );
             var orderRepository = new OrderRepository(connection);
 
             var action = () => orderRepository.GetByOrderNumber("ORD000000").Result;
