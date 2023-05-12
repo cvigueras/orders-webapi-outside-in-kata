aaa716b8ddbe10016bb79dfd124838236ba2a762
cvigueras <carlosj.vigueras@gmail.com>
2023-05-12 14:49:01 +0200

    Feat: Implemented  MediaTR with Sender in OrderController.

--- OrdersWeb.Api/Controllers/OrdersController.cs
+++ OrdersWeb.Api/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using OrdersWeb.Api.Commands;
 using OrdersWeb.Api.Models;
@@ -11,33 +12,29 @@ namespace OrdersWeb.Api.Controllers;
 public class OrdersController : ControllerBase
 {
     private readonly IMapper _mapper;
-    private readonly GetOrderByNumberQueryHandler _getOrderByNumberQueryHandler;
-    private readonly CreateOrderCommandHandler _createOrderCommandHandler;
-    private readonly UpdateOrderCommandHandler _updateOrderCommandHandler;
+    private readonly ISender _sender;
 
-    public OrdersController(IOrderRepository orderRepository, IMapper mapper)
+    public OrdersController(IMapper mapper, ISender sender)
     {
         _mapper = mapper;
-        _getOrderByNumberQueryHandler = new GetOrderByNumberQueryHandler(orderRepository, _mapper);
-        _createOrderCommandHandler = new CreateOrderCommandHandler(orderRepository, _mapper);
-        _updateOrderCommandHandler = new UpdateOrderCommandHandler(orderRepository, _mapper);
+        _sender = sender;
     }
 
     [HttpPost]
     public async Task<IActionResult> Post(OrderCreateDto orderCreateDto)
     {
         var createOrderCommand = new CreateOrderCommand(orderCreateDto);
-        await _createOrderCommandHandler.Handle(createOrderCommand, default);
+        await _sender.Send(createOrderCommand);
         //TODO RETURN NUMBER
         return Ok("ORD878878");
     }
 
     [HttpGet("{number}")]
-    public async Task<OrderReadDto> Get(string number)
+    public async Task<IActionResult> Get(string number)
     {
         var query = new GetOrderByNumberQuery(number);
-        var order = await _getOrderByNumberQueryHandler.Handle(query, default);
-        return _mapper.Map<OrderReadDto>(order);
+        var order = await _sender.Send(query);
+        return Ok(_mapper.Map<OrderReadDto>(order));
     }
 
     [HttpPut("{number}")]
@@ -46,7 +43,7 @@ public class OrdersController : ControllerBase
         if (number != orderUpdate.Number)
             return BadRequest("Order number mismatch");
         var query = new UpdateOrderCommand(orderUpdate);
-        await _updateOrderCommandHandler.Handle(query, default);
+        await _sender.Send(query);
         return Ok("Order updated successfully!");
     }
 }
