493881feca7cec3daafe635ba8f49b157a03b3bd
cvigueras <carlosj.vigueras@gmail.com>
2023-05-15 15:01:53 +0200

    Test: GetAllProductList

---
+++ OrdersWeb.Test/ProductGetFeature.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace OrdersWeb.Test
+{
+    public class ProductGetFeature
+    {
+        private HttpClient? _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = new StartupTest().CreateClient();
+        }
+
+        [Test]
+        public async Task GetAllProductList()
+        {
+            var response = await _client!.GetAsync("/Products/");
+            response.EnsureSuccessStatusCode();
+
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            await Verify(result);
+        }
+    }
+}
