63133fa8934b8c220728e8e1a6549b3009d4ad33
cvigueras <carlosj.vigueras@gmail.com>
2023-05-16 15:45:55 +0200

    Test: UpdateAnExistingOrderByNumberAfterPost

---
+++ OrdersWeb.Test/Orders/OrderingAddProductsFeature.cs
@@ -0,0 +1,53 @@
+﻿using OrdersWeb.Test.Start;
+using System.Text;
+
+namespace OrdersWeb.Test.Orders
+{
+    public class OrderingAddProductsFeature
+    {
+        private HttpClient? _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = new StartupTest().CreateClient();
+        }
+
+        [Test]
+        public async Task UpdateAnExistingOrderByNumberAfterPost()
+        {
+            var jsonPost = await GivenPostJson();
+            var jsonPut = await GivenPutJson();
+            var response = await _client!.PostAsync("/Orders/",
+                new StringContent(jsonPost,
+                    Encoding.Default,
+                    "application/json"));
+            response.EnsureSuccessStatusCode();
+
+            response = await _client!.PutAsync("/Orders/ORD765190",
+                new StringContent(jsonPut,
+                    Encoding.Default,
+                    "application/json"));
+            response.EnsureSuccessStatusCode();
+
+            response = await _client.GetAsync("/Orders/ORD765190");
+            response.EnsureSuccessStatusCode();
+
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            await Verify(result);
+        }
+
+        private async Task<string> GivenPutJson()
+        {
+            using var jsonReader = new StreamReader("./SampleData/OrderProducts.json");
+            return await jsonReader.ReadToEndAsync();
+        }
+
+        private async Task<string> GivenPostJson()
+        {
+            using var jsonReader = new StreamReader("./SampleData/Order.json");
+            return await jsonReader.ReadToEndAsync();
+        }
+    }
+}
