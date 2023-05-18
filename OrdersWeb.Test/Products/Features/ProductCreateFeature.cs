51e27580b5aea2dca007c97e2b210d26f08a1639
cvigueras <carlosj.vigueras@gmail.com>
2023-05-18 16:15:50 +0200

    Test: GetAProductByIdAfterPost

--- OrdersWeb.Test/Products/Features/ProductCreateFeature.cs
+++ OrdersWeb.Test/Products/Features/ProductCreateFeature.cs
@@ -1,7 +1,28 @@
-﻿namespace OrdersWeb.Test.Products.Features
+﻿using OrdersWeb.Test.Startup;
+
+namespace OrdersWeb.Test.Products.Features
 {
     public class ProductCreateFeature
     {
+        private Client _client;
 
+        [SetUp]
+        public void Setup()
+        {
+            new SetupFixture().CreateClient();
+            _client = new Client();
+        }
+
+        [Test]
+        public async Task GetAProductByIdAfterPost()
+        {
+            var jsonPost = await _client.GetJsonContent("./Products/Fixtures/Product.json");
+            await _client.Post(jsonPost, "/Products/");
+
+            var response = await _client.Get("/Orders/ORD765190");
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            await Verify(result);
+        }
     }
 }
