6826f850882f7acb0911e6e4fc290bd3bf9b34ee
cvigueras <carlosj.vigueras@gmail.com>
2023-05-18 14:47:57 +0200

    Refactor: 3rd step to MediaTr refactor.

--- OrdersWeb.Api/Products/ProductsController.cs
+++ OrdersWeb.Api/Products/ProductsController.cs
@@ -5,14 +5,11 @@ namespace OrdersWeb.Api.Products;
 
 public class GetAllProductsListQueryHandler
 {
-    private ProductsController _productsController;
     private readonly IProductRepository _productRepository;
     private readonly IMapper _mapper;
 
-    public GetAllProductsListQueryHandler(ProductsController productsController, IProductRepository productRepository,
-        IMapper mapper)
+    public GetAllProductsListQueryHandler(IProductRepository productRepository, IMapper mapper)
     {
-        _productsController = productsController;
         _productRepository = productRepository;
         _mapper = mapper;
     }
@@ -28,15 +25,13 @@ public class GetAllProductsListQueryHandler
 [Route("[controller]")]
 public class ProductsController : ControllerBase
 {
-    private readonly IProductRepository _productRepository;
     private readonly IMapper _mapper;
     private readonly GetAllProductsListQueryHandler _getAllProductsListQueryHandler;
 
     public ProductsController(IProductRepository productRepository, IMapper mapper)
     {
-        _productRepository = productRepository;
         _mapper = mapper;
-        _getAllProductsListQueryHandler = new GetAllProductsListQueryHandler(this, productRepository, _mapper);
+        _getAllProductsListQueryHandler = new GetAllProductsListQueryHandler(productRepository,_mapper);
     }
 
     [HttpGet]
